387e880cb1052bc93d9bbae9c8f27e423dc8bef0
robertley <robert.ley94@gmail.com>
2018-04-06 10:50:29 -0400

    added admin api calls

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mysql = require('mysql');
 
 const store = require('./store')
 var cors = require('cors')
+var config = require('./config.json')
 
 const app = express()
 app.use(express.static('public'))
@@ -23,24 +24,21 @@ app.post('/createEvent', (req, res) => {
       contact: req.body.contact
     })
     .then(() => {
-      //res.addHeader('Access-Control-Allow-Origin: *')
       res.sendStatus(200)
     })
 })
-app.get('/getEvents/NewYorkCity', (req, res) => {
-    // store.getEvents({
-    //   cityId: 1
-    // })
-    // .then(() => {
-    //   res.send("hello world")
-    // })  
+app.get('/getEvents/', (req, res) => {
+
+    var cityId = req.headers.cityid
+    var upcoming = req.headers.upcoming
+    console.log(upcoming)
     console.log("Grabbing events...")
 
     var connection = mysql.createConnection({
-      host     : 
-      user     : 
-      password : 
-      port     : 
+      host     : config.host,
+      user     : config.user,
+      password : config.password,
+      port     : config.port
     })
 
     connection.connect(function(err) {
@@ -52,15 +50,120 @@ app.get('/getEvents/NewYorkCity', (req, res) => {
     )
     
     // var sql = `SELECT * FROM shelleysite.events WHERE city = 1`
-    var sql = `SELECT * FROM ebdb.test_table;`
+    if (upcoming === "true") {
+      var sql = `SELECT * FROM ebdb.events WHERE city = ${cityId} AND date >= CURDATE() ORDER BY date DESC;`
+    }
+    else {
+      var sql = `SELECT * FROM ebdb.events WHERE city = ${cityId};`
+    }
     console.log(sql)
     connection.query(sql, function (err, result) {
       if (err) throw err
       console.log("Succesfully grabbed events.")
-      res.send(result)
+      res.send(result.reverse())
     })
 
     connection.end()
+})
+
+app.get('/getEventById/', (req, res) => {
+
+  console.log(req.headers)
+
+  var eventId = req.headers.eventid
+  console.log(`Grabbing event with id ${eventId}...`)
+
+  var connection = mysql.createConnection({
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    port     : config.port
+  })
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return
+    }
+    console.log('Connected to database.')}
+  )
+  
+  var sql = `SELECT * FROM ebdb.events WHERE id = ${eventId};`
+
+  console.log(sql)
+  connection.query(sql, function (err, result) {
+    if (err) throw err
+    console.log(`Succesfully grabbed event with id ${eventId}.`)
+    res.send(result)
+  })
+
+  connection.end()
+})
+
+app.get('/signinadmin/', (req, res) => {
+
+  var password = req.headers.password
+  console.log(`Attempting signin with password: ${password}...`)
+  if (password === "password")
+    res.send({signedIn: true, token: "a93ifnv592podjg20dfn5k"})
+  else
+    res.send({signedIn: false})
+})
+
+app.get('/checksigninadmin/', (req, res) => {
+
+  var token = req.headers.token
+  console.log(`Checking signin with password: ${token}...`)
+  if (token === "a93ifnv592podjg20dfn5k")
+    res.send({signedIn: true, token: "a93ifnv592podjg20dfn5k"})
+  else
+    res.send({signedIn: false})
+})
+
+app.get('/getAdminInfo/', (req, res) => {
+
+  console.log(`Grabbing Admin Info`)
+
+  var connection = mysql.createConnection({
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    port     : config.port
+  })
+
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return
+    }
+    console.log('Connected to database.')}
+  )
+  
+  var counts = []
+
+  // for(i = 1; i < 3; i++) {
+  //   var sql = `SELECT COUNT(*) AS count FROM ebdb.events WHERE city = '${i}' UNION;`
+
+  //   console.log(sql)
+  //   connection.query(sql, function (err, result) {
+  //     if (err) throw err
+  //     console.log(`Succesfully grabbed count WHERE city = '${i}'.`)
+  //     console.log(result[0].count)
+  //     counts.push(result[0].count)
+  //   })
+  // }
+
+  var sql = `SELECT COUNT(*) AS count FROM ebdb.events WHERE city = '1' UNION
+            SELECT COUNT(*) AS count FROM ebdb.events WHERE city = '2';`
+
+  console.log(sql)
+  connection.query(sql, function (err, result) {
+    if (err) throw err
+    console.log(`Succesfully grabbed counts from cities.`)
+    console.log(result)
+    res.send(result)
+  })
+  connection.end()
 })
 
 app.listen(8081, () => {
--- store.js
+++ store.js
@@ -1,45 +1,15 @@
-var mysql = require('mysql');
+var mysql = require('mysql')
+var config = require('./config.json')
 
 module.exports = {
   
   createEvent ({ title, description, location, date, image, city, cause, link, contact }) {
 
-    // var connection = mysql.createConnection({
-    //   host     : 
-    //   user     : 
-    //   password : 
-    //   port     : 
-    // })
-
-    // connection.connect(function(err) {
-    //   if (err) {
-    //     console.error('Database connection failed: ' + err.stack);
-    //     return
-    //   }
-    //   console.log('Connected to database.')}
-    // )
-    
-    // var sql = `INSERT INTO shelleysite.events (title, description, location, date, image, city, cause, link, contact)
-    //           VALUES ('${title}', '${description}', '${location}', '${date}', '${image}', '${city}', '${cause}', '${link}', '${contact}');`
-    // console.log(sql)
-    // connection.query(sql, function (err, result) {
-    //   if (err) throw err
-    //   console.log("1 record inserted")
-    // })
-
-    // connection.end()
-
-    return Promise.resolve()
-  },
-
-  getEvents({ cityId }) {
-    console.log("Grabbing events...")
-
     var connection = mysql.createConnection({
-      host     : 
-      user     : 
-      password : 
-      port     : 
+      host     : config.host,
+      user     : config.user,
+      password : config.password,
+      port     : config.port
     })
 
     connection.connect(function(err) {
@@ -50,16 +20,47 @@ module.exports = {
       console.log('Connected to database.')}
     )
     
-    // var sql = `SELECT * FROM ebdb.test_table;`
-    var sql = `SELECT * FROM shelleysite.events WHERE city = ${cityId}`
+    var sql = `INSERT INTO ebdb.events (title, description, location, date, image, city, cause, link, contact)
+              VALUES ('${title}', '${description}', '${location}', '${date}', '${image}', '${city}', '${cause}', '${link}', '${contact}');`
     console.log(sql)
     connection.query(sql, function (err, result) {
       if (err) throw err
-      console.log("Succesfully grabbed events.")
+      console.log("1 record inserted")
     })
 
     connection.end()
 
     return Promise.resolve()
-  }
+  },
+
+  // getEvents({ cityId }) {
+  //   console.log("Grabbing events...")
+
+  // var connection = mysql.createConnection({
+  //   host     : config.host,
+  //   user     : config.user,
+  //   password : config.password,
+  //   port     : config.port
+  // })
+
+  //   connection.connect(function(err) {
+  //     if (err) {
+  //       console.error('Database connection failed: ' + err.stack);
+  //       return
+  //     }
+  //     console.log('Connected to database.')}
+  //   )
+    
+  //   // var sql = `SELECT * FROM ebdb.test_table;`
+  //   var sql = `SELECT * FROM shelleysite.events WHERE city = ${cityId}`
+  //   console.log(sql)
+  //   connection.query(sql, function (err, result) {
+  //     if (err) throw err
+  //     console.log("Succesfully grabbed events.")
+  //   })
+
+  //   connection.end()
+
+  //   return Promise.resolve()
+  // }
 }
